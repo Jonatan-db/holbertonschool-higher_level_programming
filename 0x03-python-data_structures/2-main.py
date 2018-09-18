4cea45c1f5ab8cf0a5bcf03a8ff70378800f288e
byn3 <bryanleung92@gmail.com>
2018-09-18 18:34:37 +0000

    added py style

--- 0x03-python-data_structures/2-main.py
+++ 0x03-python-data_structures/2-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
@@ -9,4 +8,3 @@ new_list = replace_in_list(my_list, idx, new_element)
 
 print(new_list)
 print(my_list)
-
