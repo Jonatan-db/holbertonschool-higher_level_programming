671fc0729b73e5ac537217343a75058138f47535
byn3 <bryanleung92@gmail.com>
2018-09-18 22:09:16 +0000

    checks if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* is_palindrome - checks if a singly linked list is a palindrome
+* @head: the head node passed to us
+*
+* Return: 0 if not, 1 if palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+	short i, j;
+	short count = 0;
+	int array[1024];
+
+	if (!head || !(*head))
+		return (0);
+	if ((*head)->next == NULL)
+		return (1);
+	while (*head)
+	{
+		array[count] = (*head)->n;
+		*head = (*head)->next;
+		count++;
+	}
+	for (i = 0, j = count - 1; i < count / 2; i++, j--)
+		if (array[i] != array[j])
+			return (0);
+	return (1);
+}
