373fc48a755d7553de8dc9b10252c4db36f3392c
byn3 <bryanleung92@gmail.com>
2018-09-18 19:02:14 +0000

    fixed logic to work with finding c's

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,7 @@
 def no_c(my_string):
     new = ""
     for i in my_string:
-        if i != 'c' or i != 'C':
-            new = new + i
+        if i == 'c' or i == 'C':
+            continue
+        new = new + i
     return new
