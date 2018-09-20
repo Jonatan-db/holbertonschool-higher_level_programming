999a43cf43eb2835152619ac101d2d6032df5768
byn3 <bryanleung92@gmail.com>
2018-09-20 22:06:48 +0000

    added pep8 styling

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    if all(value == None for value in a_dictionary.values()):
+    if all(value is None for value in a_dictionary.values()):
         return None
     return max(a_dictionary, key=a_dictionary.get)
