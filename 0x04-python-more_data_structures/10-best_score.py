2128eaf9f097c38197658543311648289a352a8a
byn3 <bryanleung92@gmail.com>
2018-09-20 22:19:05 +0000

    changed value string to i

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    if all(value is None for value in a_dictionary.values()):
+    if all(i is None for i in a_dictionary.values()):
         return None
     return max(a_dictionary, key=a_dictionary.get)
