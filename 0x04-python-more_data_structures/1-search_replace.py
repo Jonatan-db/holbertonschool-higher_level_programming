5cae6c7f3b33c7297ee31a0477bf73c647b684b0
byn3 <bryanleung92@gmail.com>
2018-10-01 20:45:15 +0000

    fixed a typo

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if x is search else x for x in my_list]
+#    return [replace if i == search else i for i in my_list]
+    for index, i in enumerate(my_list):
+        print(i , index)
+        if i == search:
+            my_list.insert(index, search)
+    return my_list
