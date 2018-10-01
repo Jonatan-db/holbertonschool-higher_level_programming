9c803e6dbb77c13c9f214070d0e4b1f44396d6f7
byn3 <bryanleung92@gmail.com>
2018-10-01 20:58:16 +0000

    a function that prints the xth element of a list with exception handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def safe_print_list(my_list=[], x=0):
-        count = 0
-        try:
-            for i in range(x):
-                print(my_list[i], end='')
-            print("\n")
-            return x
-        except IndexError:
-            print("\n")
-            return count
-
-
-
-
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+        print()
+        return x
+    except IndexError:
+        return i
