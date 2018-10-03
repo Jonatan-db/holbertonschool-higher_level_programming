e7a2337fe11ab70d478619bf4eaae3b9ec678e54
byn3 <bryanleung92@gmail.com>
2018-10-03 05:04:05 +0000

    fixed typo. changed 0 to 1 in index

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,7 +25,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if len(value) != 2 or value[0] < 0 or value[0] < 0 or\
+        if len(value) != 2 or value[0] < 0 or value[1] < 0 or\
                 type(value) is not tuple or\
                 type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
