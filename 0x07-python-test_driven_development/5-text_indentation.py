471103794d5c31333a4ee7ce472c708ef9f6e442
byn3 <bryanleung92@gmail.com>
2018-10-04 05:33:55 +0000

    function that prints text with indentations and newlines

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" function that prints a text with new lines """
+
+def text_indentation(text):
+    """ After each . ? and : print 2 new lines
+
+    Args:
+        text: the wall of text that we are given
+
+    Return: none, just print
+    """
+    flag = 0
+    if text is "":
+        print()
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in text:
+        if i is '.' or i is '?' or i is ':':
+            flag = 1
+            print(i)
+            print()
+        elif flag is 1 and i is ' ':
+            pass
+        else:
+            print(i, end='')
+            flag = 0
