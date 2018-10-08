b7f0f0ca9ad1edac88fe1cc9444ac7442969c046
byn3 <bryanleung92@gmail.com>
2018-10-08 21:46:12 +0000

    added class rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+""" module that creates a rectangle and initializes its features """
+
+
+class Rectangle:
+    """ inidialize with the width and height with value checks
+
+    Args:
+        width: how phat dis 4polygon gon be
+        height: how tall dis box is
+
+    Return: nonezo
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        self.__height = height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
