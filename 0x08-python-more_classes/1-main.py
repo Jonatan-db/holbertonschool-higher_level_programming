93cc92f73bd54d68c93bade62a95006b47bf69d2
byn3 <bryanleung92@gmail.com>
2018-10-09 14:55:59 +0000

    added 1 more test case

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -7,3 +7,8 @@ print(my_rectangle.__dict__)
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
+
+
+my_rectangle.width = "poop"
+my_rectangle.height = "ahh"
+print(my_rectangle.__dict__)
