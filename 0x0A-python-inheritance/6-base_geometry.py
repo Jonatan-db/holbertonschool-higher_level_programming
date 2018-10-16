2d8716ea081f1221ef501a9c1a97d8b41f4b2c52
byn3 <bryanleung92@gmail.com>
2018-10-16 03:54:06 +0000

    expands the functionality of BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module that makes the class BaseGeometry """
+
+
+class BaseGeometry:
+    """ i miss vines """
+
+    def area(self):
+        """ raises error """
+        raise Exception("area() is not implemented")
