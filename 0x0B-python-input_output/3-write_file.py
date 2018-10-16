13d42f8dd9b43ffa0db4bb6c6b93858b59f0a050
byn3 <bryanleung92@gmail.com>
2018-10-16 20:36:11 +0000

    pep8 style fixing

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
     """ writes a string into a text file """
 
-    with open(filename, mode='w', encoding = "utf-8") as f:
+    with open(filename, mode='w', encoding="utf-8") as f:
         f.write(text)
 
     return len(text)
--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -5,7 +5,7 @@
 def append_write(filename="", text=""):
     """ writes a string into a text file """
 
-    with open(filename, mode='a+', encoding = "utf-8") as f:
+    with open(filename, mode='a+', encoding="utf-8") as f:
         f.write(text)
 
     return len(text)
