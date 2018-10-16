221bf000a014b5463a1a40aaf803c8d31607da47
byn3 <bryanleung92@gmail.com>
2018-10-16 23:38:20 +0000

    adds arguments to a list

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module that contains stuff """
+
+
+from sys import argv
+import json
+
+saved = __import__('7-save_to_json_file').save_to_json_file
+loaded = __import__('8-load_from_json_file').load_from_json_file
+
+
+try:
+    listo = loaded("add_item.json")
+    listo.extend(argv[1:])
+    saved(listo, "add_item.json")
+except:
+    listo = []
+    listo.extend(argv[1:])
+    saved(listo, "add_item.json")
