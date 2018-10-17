07341f49ae7306a84437665fd5debe68d70df50b
byn3 <bryanleung92@gmail.com>
2018-10-17 01:08:07 +0000

    stripped new lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,8 +6,8 @@ def read_lines(filename="", nb_lines=0):
     """ prints up to the whole file if directed """
     with open(filename, mode='r', encoding="utf-8") as f:
         if nb_lines <= 0:
-            print(f.read())
+            print(f.read(), end='')
         else:
             while nb_lines:
-                print(f.readline())
+                print(f.readline(), end='')
                 nb_lines -= 1
