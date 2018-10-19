8bc0a72274a3c5c8598fadb91dab6f9f51ad1f73
byn3 <bryanleung92@gmail.com>
2018-10-19 16:56:45 +0000

    refactored update functionality

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,25 +9,22 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ initializer """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
-    @classmethod
     def typeChecker(self, name, value):
         """ integer validation check """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
 
-    @classmethod
     def valueWHChecker(self, name, value):
         """ width and height validation check """
         if value <= 0:
             raise ValueError("{} must be > 0".format(name))
 
-    @classmethod
     def valueXYChecker(self, name, value):
         """ x and y validation check """
         if value < 0:
@@ -44,11 +41,10 @@ class Rectangle(Base):
         self.valueWHChecker("width", value)
         self.__width = value
 
-
     @property
     def height(self):
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         """ height setter, gets validated """
@@ -56,11 +52,10 @@ class Rectangle(Base):
         self.valueWHChecker("height", value)
         self.__height = value
 
-
     @property
     def x(self):
         return self.__x
-        
+
     @x.setter
     def x(self, value):
         """ X setter, gets validated """
@@ -68,11 +63,10 @@ class Rectangle(Base):
         self.valueXYChecker("x", value)
         self.__x = value
 
-
     @property
     def y(self):
         return self.__y
-        
+
     @y.setter
     def y(self, value):
         """ Y setter, gets validated """
@@ -80,6 +74,60 @@ class Rectangle(Base):
         self.valueXYChecker("y", value)
         self.__y = value
 
-    @classmethod
     def area(self):
         return self.width * self.height
+
+    def display(self):
+        for i in range(self.y):
+            print()
+        for i in range(self.height):
+            for x in range(self.width + self.x):
+                if x >= self.x:
+                    print('#', end='')
+                else:
+                    print(' ', end='')
+            print()
+
+    def __str__(self):
+        return("[{}] ({}) {}/{} - {}/{}".format(
+            str(self.__class__.__name__), self.id, self.x,
+            self.y, self.width, self.height))
+
+    def update(self, *args, **kwargs):
+        if args is not None and len(args):
+            for index, value in enumerate(args):
+                if index is 0:
+                    self.id = value
+                elif index is 1:
+                    self.width = value
+                elif index is 2:
+                    self.height = value
+                elif index is 3:
+                    self.x = value
+                elif index is 4:
+                    self.y = value
+                elif index >= 6:
+                    raise Exception("Too many arguments")
+
+            """ This wont work for anything other than 5 args
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+
+            elif len(args):
+                raise Exception("Need 5 args:  id, width, height, x, y")
+            """
+        else:
+            for key in kwargs:
+                if key is "id":
+                    self.id = kwargs["id"]
+                elif key is "width":
+                    self.width = kwargs["width"]
+                elif key is "height":
+                    self.height = kwargs["height"]
+                elif key is "x":
+                    self.x = kwargs["x"]
+                elif key is "y":
+                    self.y = kwargs["y"]
