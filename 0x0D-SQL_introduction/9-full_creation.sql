29cc3bed41298ca3b3be350ddc28ec52fce3d44a
byn3 <bryanleung92@gmail.com>
2018-12-03 20:51:14 +0000

    Add: script that adds new table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Write a script that creates a table second_table
+-- Creates a second new table
+CREATE TABLE IF NOT EXISTS second_table (id INT NOT NULL,
+name VARCHAR(256),
+score INT);
+-- inserts data into the table
+INSERT INTO second_table VALUES (1, "John", 10);
+-- inserts data into the table
+INSERT INTO second_table VALUES (2, "Alex", 3);
+-- inserts data into the table
+INSERT INTO second_table VALUES (3, "Bobn", 14);
+-- inserts data into the table
+INSERT INTO second_table VALUES (4, "George", 8);
