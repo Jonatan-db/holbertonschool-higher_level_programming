cd9def7bc4445317cc103665c4037b6871313d8e
byn3 <bryanleung92@gmail.com>
2018-12-04 21:39:26 +0000

    Change: modified left join to just join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all cities contained in the database
-SELECT DISTINCT cities.id, cities.name, states.name FROM cities LEFT JOIN states
+SELECT DISTINCT cities.id, cities.name, states.name FROM cities JOIN states
 ON cities.id = states.id;
