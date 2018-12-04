a52ee8f37a45adab182d0360ef6978dc278dfcf1
byn3 <bryanleung92@gmail.com>
2018-12-04 21:38:02 +0000

    Add: Joined two tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,3 @@
 -- Write a script that lists all cities contained in the database
-SELECT cities.id, cities.name 'name', states.name 'name'
-FROM cities
-JOIN states
-ON cities.id = states.id
+SELECT DISTINCT cities.id, cities.name, states.name FROM cities LEFT JOIN states
+ON cities.id = states.id;
