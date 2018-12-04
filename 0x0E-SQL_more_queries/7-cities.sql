75889edfc4a14554d9afb22a8e04a5a03947ef19
byn3 <bryanleung92@gmail.com>
2018-12-04 20:43:26 +0000

    Add: put in a use

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- write a script that makes a database and a tablle
 -- make the database first
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 -- creates a table in the database
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS cities (
 id INT NOT NULL AUTO_INCREMENT UNIQUE,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
