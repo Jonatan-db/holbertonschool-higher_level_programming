dbbebd53ee6602b4b6599a3bcd8c14d25dc654fa
byn3 <bryanleung92@gmail.com>
2018-12-04 20:33:37 +0000

    Add: a new table into the database the uses foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- write a script that makes a database and a tablle
+-- make the database first
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- creates a table in the database
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+id INT NOT NULL AUTO_INCREMENT UNIQUE,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (state_id) REFERENCES states(id));
