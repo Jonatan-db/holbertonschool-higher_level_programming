04ac2b0658b9c1e7b17146dc503dbb49a8990e62
byn3 <bryanleung92@gmail.com>
2018-12-05 07:07:39 +0000

    Add: started advance

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,11 @@
 -- script that lists all genres that does not match with Dexter's
-SELECT "name
-Adventure
-Comedy
-Fantasy"
-;
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN (
+	SELECT tv_genres.name
+	FROM tv_shows
+	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name;
