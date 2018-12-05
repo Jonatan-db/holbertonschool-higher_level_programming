03f0798a1bcb42f38f7ac962cf7f389a26ac912b
byn3 <bryanleung92@gmail.com>
2018-12-05 18:25:55 +0000

    Refactored code

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,5 +2,4 @@
 -- make the database first
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- make a table and let it have default and unique constants
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
