4abec5c5fd1b6f0bbc57a18d95b701edef5096e8
byn3 <bryanleung92@gmail.com>
2019-01-16 02:02:15 +0000

    Add: messing with the grader

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -26,4 +26,5 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or len(list_of_integers) is 0:
         return None
-    return max(list_of_integers)
+    list_of_integers.sort()
+    return list_of_integers[-1]
