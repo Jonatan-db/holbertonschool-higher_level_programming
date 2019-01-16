aacc71304bb39b6df82362298e251ab9b054546b
byn3 <bryanleung92@gmail.com>
2019-01-16 03:41:42 +0000

    Add: refactored and took pout the set built in

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" 
+"""
     script that does binary search on an array to find peak
 
-   ***** LOLOLOL THIS CODE WORKS AND PASSES CHECKER *****
+    ***** LOLOLOL THIS CODE WORKS AND PASSES CHECKER *****
      if list_of_integers is None or len(list_of_integers) is 0:
         return None
     list_of_integers.sort()
@@ -11,17 +11,32 @@
     ***** TROLOLOLOLOLOLLLL HAIIIIII HAHAHAIWEHAWOIEH *****
 """
 
+
 def find_peak(list_of_integers):
     """
     This is finding a peak, any peak. lol just use max or sort
     """
     if list_of_integers is None or len(list_of_integers) is 0:
         return None
-    if len(list_of_integers) is 1 or len(set(list_of_integers)) is 1:
+    if len(list_of_integers) is 1:
         return list_of_integers[0]
     mid = int(len(list_of_integers) / 2)
     l = list_of_integers[0]
     r = list_of_integers[-1]
+    if l > list_of_integers[1]:
+        return l
+    if r > list_of_integers[-2]:
+        return r
+    if list_of_integers[mid] < list_of_integers[mid+1]:
+        # if mid is less than next then we want to recurse right side
+        return find_peak(list_of_integers[mid+1:])
+    if list_of_integers[mid] < list_of_integers[mid-1]:
+        # if mid is less than the one before
+        return find_peak(list_of_integers[:mid])
+    # else if mid is bigger than or equal to both then that is a peak
+    return list_of_integers[mid]
+    """
+    SCRAPED THIS
     if len(list_of_integers) >= 3:
         if (list_of_integers[mid-1] < list_of_integers[mid]) and (
                 list_of_integers[mid] > list_of_integers[mid+1]):
@@ -39,3 +54,4 @@ def find_peak(list_of_integers):
     else:
         # print("I CAUGHT A POKEMON")
         return list_of_integers[1]
+    """
