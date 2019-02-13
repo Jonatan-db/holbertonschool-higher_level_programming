bbfe342b3d3383f5a99d7de6444bacef7eae316c
byn3 <bryanleung92@gmail.com>
2019-02-13 07:01:55 +0000

    Refactored logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (!isNaN(process.argv[3])) {
   for (let i = 2; i < process.argv.length; i++) {
     array[i - 2] = (process.argv[i]);
   }
-  array.sort(function (a, b) { return a - b; }).reverse();
+  array.sort(function (a, b) { return b - a; });
   console.log(array[1]);
 } else {
   console.log(0);
