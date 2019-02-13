cf2dd48133e6a3d1123eb7bf1b5d307db455ebfa
byn3 <bryanleung92@gmail.com>
2019-02-13 05:28:27 +0000

    Add: script that sorts reverses and gets 2nd max

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let array = []
+if (!isNaN(process.argv[3])) {
+  for (let i = 2; i < process.argv.length; i++) {
+    array[i-2] = Number(process.argv[i]);
+  }
+  array.sort().reverse();
+  console.log(array[1]);
+} else {
+  console.log(0);
+}
