ae75383ab250f2438fa68661138e3ad563b2aec2
byn3 <bryanleung92@gmail.com>
2019-02-13 05:30:40 +0000

    Add: semistandard code styling

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let array = []
+let array = [];
 if (!isNaN(process.argv[3])) {
   for (let i = 2; i < process.argv.length; i++) {
-    array[i-2] = Number(process.argv[i]);
+    array[i - 2] = Number(process.argv[i]);
   }
   array.sort().reverse();
   console.log(array[1]);
