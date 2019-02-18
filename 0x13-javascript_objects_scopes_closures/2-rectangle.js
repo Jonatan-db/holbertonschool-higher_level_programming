402edddf644f481bfe94d1092c4b2c7851e3d6c0
byn3 <bryanleung92@gmail.com>
2019-02-18 23:01:27 +0000

    Add: a is not a number check

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || arguments.length < 2) {
+    if (w <= 0 || h <= 0 || arguments.length < 2 || isNaN(w) || isNaN(h)) {
 
     } else {
       this.width = w;
