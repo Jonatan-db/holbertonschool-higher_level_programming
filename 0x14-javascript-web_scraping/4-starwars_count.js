0223893f6ee921433a7db7a4324be5a7d4a893be
byn3 <bryanleung92@gmail.com>
2019-02-19 20:53:55 +0000

    Add: iqwnroiawoniawei count i had console in wrogn spot

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,40 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fake = "http://swapi.co/api/films/";
-const fake2 = process.argv[2];
-const url = 'http://swapi.co/api/' + 'people/18';
-request(url, function (error, response, body) {
+const fake = 'http://swapi.co/api/films/';
+let count = 0;
+let charId = /18/;
+request(fake, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const val = JSON.parse(body);
-    console.log(val.films.length);
+    // console.log(val);
+    let results = val.results;
+    // console.log(results);
+    for (let i of results) {
+      // console.log(i);
+      let listCharacters = i.characters;
+      //  console.log(listCharacters);
+      for (let j in listCharacters) {
+        // console.log(listCharacters[j]);
+        if (charId.test(listCharacters[j]) === true) {
+          count++;
+        }
+      }
+    }
   }
+  console.log(count);
 });
+
+/*
+Okay this works but the intranet and grader is a butt
+request(charId, function (error, response, body) {
+if (error) {
+console.log(error);
+} else {
+const val = JSON.parse(body);
+console.log(val.films.length);
+}
+});
+*/
