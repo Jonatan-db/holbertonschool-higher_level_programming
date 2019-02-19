d239202703f5d063254f147c47949278f2bdf28e
byn3 <bryanleung92@gmail.com>
2019-02-19 23:36:19 +0000

    Add: minor fixes

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 const request = require('request');
 request(process.argv[2], 'utf-8', function (error, response, body) {
   if (error) {
-    console.log(error);
+    throw (error);
   } else {
     let data = JSON.parse(body);
     let dict = {};
