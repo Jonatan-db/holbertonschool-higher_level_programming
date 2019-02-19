48482032876461477e3ba7637f0b040675f95d6a
byn3 <bryanleung92@gmail.com>
2019-02-19 23:43:47 +0000

    Add: refactored logic

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,32 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], 'utf-8', function (error, response, body) {
-  let dict = {};
+request(process.argv[2], function (error, response, body) {
   if (error) {
-    throw (error);
+    console.log(error);
   } else {
     let data = JSON.parse(body);
 
+    let dict = {};
     for (let i = 0; i < data.length; i++) {
       let user = data[i];
-      if (dict[user.userId] === undefined) {
-        dict[user.userId] = 0;
-      }
+
       if (user.completed === true) {
-        dict[user.userId]++;
+        if (user.userId in dict) {
+          dict[user.userId] += 1;
+        } else {
+          dict[user.userId] = 1;
+        }
       }
+
+      /*
+if (dict[user.userId] === undefined) {
+dict[user.userId] = 0;
+}
+if (user.completed === true) {
+dict[user.userId]++;
+}
+*/
     }
+    console.log(dict);
   }
-  console.log(dict);
 });
