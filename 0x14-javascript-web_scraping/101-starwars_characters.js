8baae2ab47413de3a3aeddcc22dcbb4d94fbe2dc
byn3 <bryanleung92@gmail.com>
2019-02-20 16:16:43 +0000

    Add: semistandard styling

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,26 +8,25 @@ request(url, function (error, response, body) {
   } else {
     let data = JSON.parse(body).characters;
     // console.log(data);
-	let order = []
+    let order = [];
     for (let i = 0; i < data.length; i++) {
-	  order.push(new Promise(function(resolve, reject) {
-
+      order.push(new Promise(function (resolve, reject) {
         request.get(data[i], function (error, response, body) {
-        if (error) {
-          reject(error);
-        }
-        resolve(JSON.parse(body).name);
-      }); 
-    }));
-	// console.log(order[i]);
-	}
-	Promise.all(order).then(character => {
-	  for (let j = 0; j < character.length; j++) {
-	    console.log(character[j]);
-	  }
-	  // can shorten above 2 lines into this 1 line below 
-	  // character.forEach(person => console.log(person));
-	});
-	// console.log(order);
+          if (error) {
+            reject(error);
+          }
+          resolve(JSON.parse(body).name);
+        });
+      }));
+      // console.log(order[i]);
     }
+    Promise.all(order).then(character => {
+      for (let j = 0; j < character.length; j++) {
+        console.log(character[j]);
+      }
+      // can shorten above 2 lines into this 1 line below
+      // character.forEach(person => console.log(person));
+    });
+    // console.log(order);
+  }
 });
