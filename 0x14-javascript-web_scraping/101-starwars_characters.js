a22c9002698e7f622d2466c4f807bd78d3049b30
byn3 <bryanleung92@gmail.com>
2019-02-20 16:09:59 +0000

    Add: script that keeps order when fetching via PROMISES

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,21 +7,27 @@ request(url, function (error, response, body) {
     console.log(error);
   } else {
     let data = JSON.parse(body).characters;
-
+    // console.log(data);
+	let order = []
     for (let i = 0; i < data.length; i++) {
-      request.get(data[i], function (error, response, body) {
-        if (error) {
-          throw error;
-        }
-        console.log(JSON.parse(body).name);
-      });
-    }
-    // console.log(data['name']);
+	  order.push(new Promise(function(resolve, reject) {
 
-    // data.forEach(name => {
-    // request.get(name, function(error, response, body) {
-    // console.log(JSON.parse(body).name);
-    // });
-    // });
-  }
+        request.get(data[i], function (error, response, body) {
+        if (error) {
+          reject(error);
+        }
+        resolve(JSON.parse(body).name);
+      }); 
+    }));
+	// console.log(order[i]);
+	}
+	Promise.all(order).then(character => {
+	  for (let j = 0; j < character.length; j++) {
+	    console.log(character[j]);
+	  }
+	  // can shorten above 2 lines into this 1 line below 
+	  // character.forEach(person => console.log(person));
+	});
+	// console.log(order);
+    }
 });
