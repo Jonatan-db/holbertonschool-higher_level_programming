b7797ba72d27106f9b7e87b2f6e9cc3db59702ed
Bryan Leung <bryanleung92@gmail.com>
2019-02-21 12:05:08 -0800

    Add: script that manipulates the DOM

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,1 @@
-<head>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    document.querySelector("header").style.color= "#FF0000";
-</head>
+document.querySelector("header").style.color= "red";
