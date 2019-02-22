9b330094a3bf6532c1c4dade60e59339f2a15ec8
Bryan Leung <bryanleung92@gmail.com>
2019-02-22 08:39:45 -0800

    Push advance provlems

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  const valid = ['ar', 'az', 'be', 'bg', 'bn', 'bs', 'cs', 'da', 'de', 'dz',
+  'el', 'en', 'en-gb', 'en-us', 'es', 'et', 'fa', 'fi', 'fil', 'fr', 'he',
+  'hi', 'hr', 'hu', 'hy', 'id', 'is', 'it', 'ja', 'ka', 'kk', 'km', 'ko', 'lb',
+  'lo', 'lt', 'lv', 'mk', 'mn', 'ms', 'my', 'ne', 'no', 'pl', 'pt', 'ro', 'ru',
+  'sk', 'sl', 'sq', 'sr', 'sv', 'sw', 'th', 'tk', 'uk', 'vi', 'zh'];
+
+  $('#btn_translate').on('click', function () {
+    const url = 'https://www.fourtonfish.com/hellosalut/?lang=';
+    let input = url + $('#language_code').val();
+    $.get(input, function (data) {
+	  if (data.code === 'none') {
+	     $('#hello').text('Bad input. Please input any of the following ' + valid);
+	  }
+	  else{
+      $('#hello').text(data.hello);
+      }
+    });
+  });
+
+  $(document).keypress(function (e) {
+   if (e.which === 13) {
+    const url = 'https://www.fourtonfish.com/hellosalut/?lang=';
+    let input = url + $('#language_code').val();
+    $.get(input, function (data) {
+	  if (data.code === 'none') {
+	     $('#hello').text('Bad input, Please input any of the following ' + valid);
+	  }
+	  else{
+      $('#hello').text(data.hello);
+      }
+    });
+	}
+  });
+});
