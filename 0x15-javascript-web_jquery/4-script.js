2327ac82fe3904d14dc6e752bcbed826686dc44d
Bryan Leung <bryanleung92@gmail.com>
2019-02-22 08:49:30 -0800

    Change: fixed errors to match expected intranet inputs

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,10 +1,11 @@
-$('header').on('click', function () {
-  if ($(this).hasClass('red')) {
+$('#toggle_header').on('click', function () {
+
+  if ($('header').hasClass('red')) {
     // $(this).toggleClass('red');
-    $(this).removeClass('red');
-    $(this).addClass('green');
-  } else if ($(this).hasClass('green')) {
-    $(this).removeClass('green');
-    $(this).addClass('red');
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  } else if ($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
   }
 });
